cbfac602931bf951b737065e82a1c224d41d9e29
jrodrigo340 <joaorodrigooliver9@gmail.com>
2025-07-25 20:39:41 -0300

    Finalização do desafio.

--- ContaBanco/src/ComntaTerminal.java
+++ ContaBanco/src/ComntaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class ComntaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite os 4 ultimos número do cartão: ");
+        int numero = scanner.nextInt();
+
+        System.out.print("Digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+        scanner.nextLine();
+
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Digite seu saldo atual: R$ ");
+        Float saldo = scanner.nextFloat();
+
+        
+        System.out.printf("\nOlá %s! Obrigado por criar sua conta conosco.\n", nome);
+        System.out.printf("Agência: %s\n", agencia);
+        System.out.printf("Número da Conta: %d\n", numero);
+        System.out.printf("Saldo: R$%.2f - já disponível para saque.\n", saldo);
+
     }
 }
