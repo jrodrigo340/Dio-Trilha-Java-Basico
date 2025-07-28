e9a5a43ab373b846b497266a00856382a1d4de33
jrodrigo340 <joaorodrigooliver9@gmail.com>
2025-07-27 23:37:58 -0300

    Subindo desafio da Dio

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ComntaTerminal {
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
